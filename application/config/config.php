852f6987fd0a21e78da3e314ccb0ec82ea315f5b
Alejandro <alexro2404@hotmail.com>
2015-10-30 17:34:54 +0000

    Configurando ejemplo rest server y configuracion de base de datos

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'centoswebserverapps.cloudapp.net/serverRest';
+$config['base_url'] = 'http://centoswebserverapps.cloudapp.net/serverRest/';
 
 /*
 |--------------------------------------------------------------------------
